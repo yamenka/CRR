6392bfb8c9b99052f786d6ee6fc98144881031e1
yamenka <74372862+yamenka@users.noreply.github.com>
2023-12-09 21:34:28 +0000

    Update SRR.py

--- SRR.py
+++ SRR.py
@@ -1,20 +1,20 @@
 # Sample Input
-processes = [["p1", 7, 0], ["p2", 5, 1], ["p3", 4, 3], ["p4", 2, 4]]
+processes = [["p1", 6, 0], ["p2", 5, 1], ["p3", 4, 4], ["p4", 2, 3]]
 
 # Initialization
 service_times = [p[1] for p in processes]
 arrival_times = [p[2] for p in processes]
-priorities = [0 for _ in processes]  # Initial priorities all set to 0
+priorities = [0] * len(processes)  # Initial priorities all set to 0
 new_queue = []
 accepted_queue = []
 status_queue = ['-', '-', '-', '-']
 T = 0
-q = 2
+q = 1
 
 # Helper function to update status
 def update_status():
     for i in range(len(status_queue)):
-        if status_queue[i] != 'F':
+        if status_queue[i] not in {'F', 'W'}:
             status_queue[i] = '-'
     for p in new_queue:
         status_queue[processes.index(p)] = 'W'
@@ -29,11 +29,9 @@ while service_times != [0,0,0,0]:
         if p[2] == T:
             if not accepted_queue:
                 accepted_queue.append(p)
+                status_queue[processes.index(p)] = 'R'
             else:
                 new_queue.append(p)
-    # print(f"Accepted Queue{accepted_queue}")
-    # print(f"New Queue {new_queue}")
-    # print(f"Services{service_times}")
 
     # Update priorities
     # For new queue
@@ -45,9 +43,20 @@ while service_times != [0,0,0,0]:
     for p in accepted_queue:
         p_index = processes.index(p)
         priorities[p_index] += 1
-    
+
+    print(f"T = {T}, Status Queue: {status_queue}, Priorities: {priorities},{service_times}")
 
     # Process execution in accepted queue
+
+    
+      
+
+    # Move processes between queues
+    for p in new_queue:
+        p_index = processes.index(p)
+        if accepted_queue and priorities[p_index] >= min([priorities[processes.index(proc)] for proc in accepted_queue]):
+            new_queue.remove(p)
+            accepted_queue.append(p)  # Add as the second element
 
     if accepted_queue:
         running_process = accepted_queue.pop(0)
@@ -60,35 +69,15 @@ while service_times != [0,0,0,0]:
         if service_times[process_index] <= 0:
             status_queue[process_index] = 'F'
         else:
-      
             accepted_queue.append(running_process)  # Insert at index 1
 
-    # Move processes between queues
-    for p in new_queue[:]:
-        p_index = processes.index(p)
-        if accepted_queue and priorities[p_index] >= min(priorities[processes.index(proc)] for proc in accepted_queue):
-            new_queue.remove(p)
-            accepted_queue.append(p)  # Add as the second element
+
 
     # Update statuses
-
-
     update_status()
 
-
-    # Print status and priorities for each time step
-    print(f"T = {T}, Status Queue: {status_queue}, Priorities: {priorities},{service_times}")
-
-
-    # Increment Time
     T += 1
-
-
-
-    
-
-    
-
 
 # Output final status
 print("Final Status Queue:", status_queue)
+print(f"T = {T}, Status Queue: {status_queue}, Priorities: {priorities},{service_times}")
